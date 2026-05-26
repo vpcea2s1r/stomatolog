dd7ec2aa4d754bc48a1a7db39e720514be8e3b43
vpcea2s1r <35611186+vpcea2s1r@users.noreply.github.com>
2026-05-26 22:17:44 +0300

    clear test domain

--- src/util/slug.ts
+++
@@ -1,7 +0,0 @@
-export function slugFromPath(pathname: string): string {
-  return pathname.replace(/\/$/, '').split('/').pop() || '';
-}
-
-export function isActivePage(current: string, target: string): boolean {
-  return current === target || current.startsWith(target + '/');
-}
